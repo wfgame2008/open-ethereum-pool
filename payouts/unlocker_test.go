aa1cb53540ecbd2259a57f59316c19cdbb8834a4
wfgame2008 <wfgame2008@qq.com>
2017-11-23 14:41:32 +0800

    Update unlocker_test.go

--- payouts/unlocker_test.go
+++ payouts/unlocker_test.go
@@ -5,8 +5,8 @@ import (
 	"os"
 	"testing"
 
-	"github.com/sammy007/open-ethereum-pool/rpc"
-	"github.com/sammy007/open-ethereum-pool/storage"
+	"github.com/wfgame2008/open-ethereum-pool/rpc"
+	"github.com/wfgame2008/open-ethereum-pool/storage"
 )
 
 func TestMain(m *testing.M) {

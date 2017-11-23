e30bed1c3569ac63335d21d7837296b032a0e69c
wfgame2008 <wfgame2008@qq.com>
2017-11-23 14:40:40 +0800

    Update payer.go

--- payouts/payer.go
+++ payouts/payer.go
@@ -10,9 +10,9 @@ import (
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 
-	"github.com/sammy007/open-ethereum-pool/rpc"
-	"github.com/sammy007/open-ethereum-pool/storage"
-	"github.com/sammy007/open-ethereum-pool/util"
+	"github.com/wfgame2008/open-ethereum-pool/rpc"
+	"github.com/wfgame2008/open-ethereum-pool/storage"
+	"github.com/wfgame2008/open-ethereum-pool/util"
 )
 
 const txCheckInterval = 5 * time.Second
